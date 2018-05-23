1db604ce200eeba28fbba27019ea9f6150ca090a
David Smelt <davdms@gmail.com>
2018-05-23 14:28:18 +0200

    plot latency
    Change-Id: Ie7cd7bcd467e614617a2872754240f1a3da5ab0e

---
+++ plotlatency.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+#
+# Plot latency numbers for simulation stats in outdir to latency_file
+
+import os
+import sys
+
+## Parse stats.txt for the specified key and return the associated value as float
+def getStatsForString(stats_file, key):
+    with open(stats_file, "rt") as f:
+        for line in f:
+            if key in line:
+                split = line.split()
+                return float(split[-1])
+    return 0.0
+
+if len(sys.argv) < 4:
+    print("Usage: %s <simulation directory> " \
+          "<latency output file> <injection rate>" % sys.argv[0])
+
+outdir = sys.argv[1]
+latency_file = sys.argv[2]
+injrate = float(sys.argv[3])
+stats_file = os.path.join(outdir, "stats.txt")
+
+latency = getStatsForString(stats_file, "system.ruby.network.average_packet_latency")
+
+with open(latency_file, "a") as f:
+    f.write("{0:f}   {1:f}\n".format(injrate, latency))
